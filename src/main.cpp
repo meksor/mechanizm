0f28441e1fb678a25cfbb757eecd9cf1669d5c76
Max Wolschlager <comms@meks.io>
2020-10-09 13:47:02 +0200

    fix cmake

--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,19 @@
 
 int main(int argc, char* argv[])
 {
-  FFmpegReader r(argv[1]);
+  int frame = 0;
+  openshot::FFmpegReader r(argv[1]);
   r.Open(); // Open the reader
   // Get frame number 1 from the video
-  std::shared_ptr<Frame> f = r.GetFrame(1);
-  // Now that we have an openshot::Frame object, lets have some fun!
-  f->Display(); // Display the frame on the screen
-  f->DisplayWaveform(); // Display the audio waveform as an image
-  f->Play(); // Play the audio through your speaker
-  // Close the reader
+  std::shared_ptr<openshot::Frame> f;
+  while (f = r.GetFrame(frame)) { 
+    // Now that we have an openshot::Frame object, lets have some fun!
+    f->Display(); // Display the frame on the screen
+    // f->DisplayWaveform(); // Display the audio waveform as an image
+    // f->Play(); // Play the audio through your speaker
+    // Close the reader
+    frame += 1;
+  }
   r.Close();
   return 0;
 }
